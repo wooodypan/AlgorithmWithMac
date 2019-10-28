bed8f53fe2a489d0b5ade208a2956b68b588480a
WeirdPan <948567749@qq.com>
2019-10-28 22:25:50 +0800

    swift tree insert

--- AlgorithmWithMac/Swift/PPCodeEntry.swift
+++ AlgorithmWithMac/Swift/PPCodeEntry.swift
@@ -11,11 +11,18 @@ import Foundation
 
 open class PPCodeEntry : NSObject {
     public override init() {
-        let tree = PPSearchTree.init()
-        tree.insert(num: 4)
+        
+        
+        
+        
     }
     
     @objc func printLog() -> Void {
         print("Swft算法入口")
+        let tree = PPSearchTree.init(rootNode: nil)
+        tree.insert(4)
+        tree.insert(1)
+        tree.insert(2)
+        tree.insert(5)
     }
 }
--- AlgorithmWithMac/Swift/PPTree.swift
+++ AlgorithmWithMac/Swift/PPTree.swift
@@ -10,8 +10,8 @@ import Foundation
 
 class PPSearchTreeNode: NSObject {
     var value = 0
-    var left:PPSearchTreeNode? = nil;
-    var right:PPSearchTreeNode? = nil;
+    var leftNode:PPSearchTreeNode? = nil;
+    var rightNode:PPSearchTreeNode? = nil;
 
     
     init(value:Int) {
@@ -23,10 +23,77 @@ class PPSearchTreeNode: NSObject {
 
 
 open class PPSearchTree: NSObject {
-    open func insert(num:Int) -> Void {
-        print("insert num=\(num)")
+    var rootNode:PPSearchTreeNode? = nil;
+    
+    init(rootNode:PPSearchTreeNode?) {
+        self.rootNode = rootNode
     }
     
+    func insert(_ num:Int) -> Void {
+        let node = PPSearchTreeNode.init(value: num)
+        if (self.rootNode == nil) {
+            self.rootNode = node;
+            return
+        }
+        if let prent = getPrev(num: num, find: false) {
+            if (num < prent.value) {
+                prent.leftNode = node;
+            } else {
+                prent.rightNode = node;
+            }
+            print("insert num=\(num)")
+        }
+    }
+    
+    func getPrev(num:Int,find:Bool) -> PPSearchTreeNode? {
+        let point = self.rootNode
+        var res:Array<PPSearchTreeNode> = []
+        if let point = point {
+            
+        var newPoint:PPSearchTreeNode! = point
+        while (true) {
+            if ((newPoint.leftNode) != nil) {
+                if (num < point.leftNode?.value ?? 0 || num < point.value) {
+                    newPoint = point.leftNode
+                    continue
+                }
+            }
+
+            if ((newPoint.rightNode) != nil) {
+                if (num >= point.rightNode?.value ?? 0 || num >= point.value) {
+                    //搜索时如果有多个值则缓存
+                    if (find && num == point.value) {
+                        res.append(point);
+                    }
+                    newPoint = point.rightNode;
+                    continue
+                }
+            }
+            //如果是搜索
+            if (find) {
+                if (point.value == num) {
+                    res.append(point);
+                }
+
+                if (res.count == 0) {
+                    return nil
+                }
+
+                if (res.count == 1) {
+                    return res[0];
+                }
+                #warning("这里应该是数组res")
+                return res[0];
+            }
+            //如果是添加 返回的是应该添加的那各节点的父节点
+            return newPoint;
+        }
+        
+        
+        }
+        return nil
+        
+    }
     
     
 }
